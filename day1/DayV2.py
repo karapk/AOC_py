a5dce980172eb1142d0ecdfb50cb59fc4531242f
Philip K <100199180+karapk@users.noreply.github.com>
2025-12-16 16:00:36 -0600

    setup day1 step 2

---
+++ day1/DayV2.py
@@ -0,0 +1,37 @@
+currentlocation = 50
+
+counter = 0
+
+
+def left_turn(currentlocation, turn_value):
+        new_location = currentlocation - turn_value
+        while new_location < 0:
+            new_location = 100 + new_location
+        return new_location
+
+def right_turn(currentlocation, turn_value):
+    new_location = currentlocation + turn_value
+    while new_location > 99:
+        new_location = new_location - 100
+    return new_location
+
+with open("day1/sampledata.txt", "r") as file:
+
+    for line in file:
+        
+        # Check for 'L' (Left Turn) 
+        if "L" in line:
+            leftvalue = int(line.strip("L"))
+            currentlocation = left_turn(currentlocation, leftvalue)
+            if currentlocation == 0:
+                counter += 1
+                
+        # Check for 'R' (Right Turn)
+        elif "R" in line:
+            rightvalue = int(line.strip("R"))
+            currentlocation = right_turn(currentlocation, rightvalue)
+            if currentlocation == 0:
+                counter += 1
+                
+print(f"Final counter value: {counter}")
+        
