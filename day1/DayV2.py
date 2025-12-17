8246a9b6a0c8b6d0353b86271088910fcd5a3364
Philip K <100199180+karapk@users.noreply.github.com>
2025-12-16 19:56:23 -0600

    current day2 working with sample data w/logs

--- day1/DayV2.py
+++ day1/DayV2.py
@@ -5,14 +5,10 @@ counter = 0
 
 def left_turn(currentlocation, turn_value):
         new_location = currentlocation - turn_value
-        while new_location < 0:
-            new_location = 100 + new_location
         return new_location
 
 def right_turn(currentlocation, turn_value):
     new_location = currentlocation + turn_value
-    while new_location > 99:
-        new_location = new_location - 100
     return new_location
 
 with open("day1/sampledata.txt", "r") as file:
@@ -28,6 +24,15 @@ with open("day1/sampledata.txt", "r") as file:
             if currentlocation == 0:
                 counter += 1
                 print(f" counter incremented due to LEFT turn to zero. New counter: {counter}")
+            elif currentlocation < 0:
+                while currentlocation < 0:
+                    print(f"currentlocation vs previouslocation: 100 - {currentlocation} ")
+                    if (previouslocation != 0) and (currentlocation <= 0):
+                        counter += 1
+                    print(f" counter incremented due to LEFT turn crossing zero. New counter: L{counter}")
+                    currentlocation = 100 + currentlocation
+                    print(f"currentlocation vs previouslocation: 100 - {currentlocation} ")
+                    
                 
         # Check for 'R' (Right Turn)
         elif "R" in line:
@@ -37,7 +42,15 @@ with open("day1/sampledata.txt", "r") as file:
             print(f" RIGHT turn from {previouslocation} to {currentlocation}!")
             if currentlocation == 0:
                 counter += 1
-                print(f" counter incremented due to RIGHT turn to zero. New counter: {counter}")
+                print(f" counter incremented due to RIGHT turn to zero. New counter: R{counter}")
+            elif currentlocation > 99:
+                while currentlocation > 99:
+                    print(f" right currentlocation vs previouslocation: {currentlocation} - 100 ")
+                    if (previouslocation != 0) and (currentlocation > 99):
+                        counter += 1
+                    print(f" counter incremented due to RIGHT turn crossing zero. New counter: R{counter}")
+                    currentlocation = currentlocation - 100
+                    print(f"currentlocation vs previouslocation: {currentlocation} - 100 ")
                 
 print(f"Final counter value: {counter}")
         
