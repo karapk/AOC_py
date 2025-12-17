eb881780df05719591a98cf4f2b48c20f56d7d91
Philip K <100199180+karapk@users.noreply.github.com>
2025-12-16 18:14:22 -0600

    add logging to see when turn happens and values

--- day1/DayV2.py
+++ day1/DayV2.py
@@ -22,16 +22,22 @@ with open("day1/sampledata.txt", "r") as file:
         # Check for 'L' (Left Turn) 
         if "L" in line:
             leftvalue = int(line.strip("L"))
+            previouslocation = currentlocation
             currentlocation = left_turn(currentlocation, leftvalue)
+            print(f" LEFT turn from {previouslocation} to {currentlocation}!")
             if currentlocation == 0:
                 counter += 1
+                print(f" counter incremented due to LEFT turn to zero. New counter: {counter}")
                 
         # Check for 'R' (Right Turn)
         elif "R" in line:
             rightvalue = int(line.strip("R"))
+            previouslocation = currentlocation
             currentlocation = right_turn(currentlocation, rightvalue)
+            print(f" RIGHT turn from {previouslocation} to {currentlocation}!")
             if currentlocation == 0:
                 counter += 1
+                print(f" counter incremented due to RIGHT turn to zero. New counter: {counter}")
                 
 print(f"Final counter value: {counter}")
         
